353f19cc3baeaf9ce800ba4d77ab352c604f5883
Yulia Mishieva <kotyasher@gmail.com>
2024-04-08 17:41:38 +0300

    Seventh Step

---
+++ src/games/gcd-game.js
@@ -0,0 +1,30 @@
+import { getRandomNumber, getGameLaunch } from '../index.js';
+
+const getGCD = (numOne, numTwo) => {
+  if (numTwo % numOne === 0) {
+    return numOne;
+  }
+  for (let i = numOne / 2; i >= 1; i -= 1) {
+    if (numTwo % i === 0 && numOne % i === 0) {
+      return i;
+    }
+  }
+  return 1;
+};
+
+const generateGCD = () => {
+  const minNum = 1;
+  const maxNum = 50;
+  const firstNumRandom = getRandomNumber(minNum, maxNum);
+  const secondNumRandom = getRandomNumber(minNum, maxNum);
+  const question = `${firstNumRandom} ${secondNumRandom}`;
+  const correctAnswer = String(getGCD(firstNumRandom, secondNumRandom));
+
+  return [question, correctAnswer];
+};
+
+const startGcdGame = () => {
+  getGameLaunch(generateGCD, 'Find the greatest common divisor of given numbers.');
+};
+
+export default startGcdGame;
